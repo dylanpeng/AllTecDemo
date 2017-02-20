6422efe6ffe733c6d8db2d48998f6176baa0ec4e
peng_dylan <peng_dylan@sina.com>
2017-02-20 21:50:50 +0800

    2017-2-20 添加Session验证登录项目

---
+++ MVCAuthorizeTest/MVCControllerAuthTest/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControllerAuthTest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            var controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+
+            var userName = Session["UserName"] as String;
+            if (String.IsNullOrEmpty(userName))
+            {
+                //重定向至登录页面
+                filterContext.Result = RedirectToAction("Index", "Login", new { url = Request.RawUrl});
+                return;
+            }
+
+        }
+    }
+}
---
+++ MVCAuthorizeTest/MVCControllerAuthTest/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControllerAuthTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index(string ReturnUrl)
+        {
+            if (Session["UserName"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Url = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string password, string returnUrl)
+        {
+            /*
+                添加验证用户名密码代码
+            */
+            Session["UserName"] = name;
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/") && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // POST: /Account/LogOff
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            Session["UserName"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
