283f85386d99adc37b4bf6bc442c48ff59113afe
peng_dylan <peng_dylan@sina.com>
2017-02-22 21:06:45 +0800

    2017-2-22 WCF服务项目实例

---
+++ WcfServiceTest/WcfClientConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WcfService.WcfServiceClient();
+            try
+            {
+                var str = client.GetData(2046);
+                Console.WriteLine(string.Format("内容:{0}", str));
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("出现异常！");
+                client.Abort();
+            }
+            Console.ReadLine();
+        }
+    }
+}
