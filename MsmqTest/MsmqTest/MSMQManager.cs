3f76475840e95a932ae808565478f97ea5b7d144
peng_dylan <peng_dylan@sina.com>
2017-02-23 22:00:22 +0800

    2017-2-23 MsmqTest project commit

---
+++ MsmqTest/MsmqTest/MSMQManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsmqTest
+{
+    public class MSMQManager
+    {
+        public string _path;
+        public MessageQueue _msmq;
+
+        public MSMQManager(string path = null)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                _path = ConfigurationManager.AppSettings["MsmqPath"].ToString();
+            }
+            else
+            {
+                _path = path;
+            }
+            if (MessageQueue.Exists(_path))
+            {
+                _msmq = new MessageQueue(_path);
+            }
+            else
+            {
+                _msmq = MessageQueue.Create(_path);
+            }
+        }
+
+        public void Send(string body)
+        {
+            _msmq.Send(new Message(body, new BinaryMessageFormatter()));
+        }
+
+        public object ReceiveMessage()
+        {
+            var msg = _msmq.Receive();
+            if (msg != null)
+            {
+                msg.Formatter = new BinaryMessageFormatter();
+                Console.WriteLine("消息内容：{0}", msg.Body);
+                return msg.Body;
+            }
+            return null;
+        }
+
+        public void WriteAllMessage()
+        {
+            var enumerator = _msmq.GetMessageEnumerator2();
+            while (enumerator.MoveNext())
+            {
+                Message msg = (Message)(enumerator.Current);
+                msg.Formatter = new BinaryMessageFormatter();
+                Console.WriteLine("消息内容：{0}", msg.Body);
+                _msmq.ReceiveById(msg.Id);
+
+            }
+            //var msgs = _msmq.GetAllMessages();
+            //for (int i = 0; i < msgs.Length; i++)
+            //{
+            //    msgs[i].Formatter = new BinaryMessageFormatter();
+            //    Console.WriteLine("消息内容：{0}", msgs[i].Body);
+            //}
+        }
+
+        //public bool Create()
+        //{
+        //    if (MessageQueue.Exists(_path))
+        //        return true;
+
+        //    if (MessageQueue.Create(_path) != null)
+        //    {
+        //        return true;
+        //    }
+        //    return false;   
+        //}
+    }
+}
---
+++ MsmqTest/MsmqTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsmqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msmqManager = new MSMQManager();
+            for (int i = 1; i <= 10; i++)
+            {
+                msmqManager.Send(string.Format("消息{0}", i));
+            }
+            var msg = msmqManager.ReceiveMessage();
+            msmqManager.WriteAllMessage();
+            Console.ReadLine();
+        }
+    }
+}
