d8ba62d16c25bcf93b6adec0394044b0bd13443b
peng_dylan <peng_dylan@sina.com>
2017-03-01 18:40:34 +0800

    2017-3-1 base32编码获得8位字符

---
+++ Base32Code/Base32Code/CBase32.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base32Code
+{
+    /// <summary>
+    /// Summary description for CBase32.
+    /// </summary>
+    public class CBase32
+    {
+        private const String DefaultBase32Map = "ABCDEFGHJKMNPRSTUVWXYZ1234567890";
+        private const Int32 Base32MapLength = 32;
+
+        private static Char[] m_acBase32Map = null;
+
+        static CBase32()
+        {
+            m_acBase32Map = DefaultBase32Map.ToCharArray();
+        }
+        public CBase32()
+        {
+        }
+
+        public static String Base32Map
+        {
+            get
+            {
+                return m_acBase32Map.ToString();
+            }
+            set
+            {
+                if (value != null && value.Length >= Base32MapLength)
+                {
+                    m_acBase32Map = value.ToCharArray();
+                }
+                else
+                {
+                    m_acBase32Map = DefaultBase32Map.ToCharArray();
+                }
+            }
+        }
+
+        public static Char GetCharacter(Int32 dwIndex)
+        {
+            Char cMappingData = '0';
+
+            if (m_acBase32Map != null && dwIndex >= 0 && dwIndex < m_acBase32Map.Length)
+            {
+                cMappingData = m_acBase32Map[dwIndex];
+            }
+            return cMappingData;
+        }
+
+
+        public static Int32 GetCharIndex(Char cData)
+        {
+            Int32 dwIndex = -1, dwLoop = 0;
+
+            if (m_acBase32Map != null)
+            {
+                for (dwLoop = 0; dwLoop < m_acBase32Map.Length; dwLoop++)
+                {
+                    if (m_acBase32Map[dwLoop] == cData)
+                    {
+                        dwIndex = dwLoop;
+                        break;
+                    }
+                }
+            }
+            return dwIndex;
+        }
+
+        public static String Encode(Byte[] abData)
+        {
+            Int32 dwLoop = 0, dwCharIndex = 0, dwCharCount;
+            Char[] acPart = null;
+            StringBuilder sbOutput = null;
+
+            if (abData == null || m_acBase32Map == null || m_acBase32Map.Length < Base32MapLength)
+                return null;
+
+            try
+            {
+                dwCharCount = (Int32)(abData.Length / 5f * 8f) + 1;
+                sbOutput = new StringBuilder(dwCharCount);
+                acPart = new Char[8];
+            }
+            catch (Exception)
+            {
+            }
+            if (acPart == null || sbOutput == null)
+                return null;
+
+            dwCharCount = 0;
+            for (dwLoop = 0; dwLoop < abData.Length; dwLoop += 5)
+            {
+                // every 5 bytes is a unit,can convert to 8 chars
+                // data format:
+                //   AAAAABBB BBCCCCCD DDDDEEEE EFFFFFGG GGGHHHHH
+                switch (abData.Length - dwLoop)
+                {
+                    case 1:
+                        dwCharIndex = abData[dwLoop] >> 3;    // AAAAA
+                        acPart[0] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop] & 0x7) << 2;  // BBB00
+                        acPart[1] = m_acBase32Map[dwCharIndex];
+                        dwCharCount = 2;
+                        break;
+
+                    case 2:
+                        dwCharIndex = abData[dwLoop] >> 3;    // AAAAA
+                        acPart[0] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop] & 0x7) << 2) + (abData[dwLoop + 1] >> 6); // BBBBB
+                        acPart[1] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 1] & 0x3F) >> 1; // CCCCC
+                        acPart[2] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = abData[dwLoop + 1] & 0x1;   // D0000
+                        acPart[3] = m_acBase32Map[dwCharIndex];
+                        dwCharCount = 4;
+                        break;
+
+                    case 3:
+                        dwCharIndex = abData[dwLoop] >> 3;    // AAAAA
+                        acPart[0] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop] & 0x7) << 2) + (abData[dwLoop + 1] >> 6); // BBBBB
+                        acPart[1] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 1] & 0x3F) >> 1; // CCCCC
+                        acPart[2] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 1] & 0x1) << 4) + (abData[dwLoop + 2] >> 4);// DDDDD
+                        acPart[3] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 2] & 0xF) << 1; // EEEE0
+                        acPart[4] = m_acBase32Map[dwCharIndex];
+                        dwCharCount = 5;
+                        break;
+
+                    case 4:
+                        dwCharIndex = abData[dwLoop] >> 3;    // AAAAA
+                        acPart[0] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop] & 0x7) << 2) + (abData[dwLoop + 1] >> 6); // BBBBB
+                        acPart[1] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 1] & 0x3F) >> 1; // CCCCC
+                        acPart[2] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 1] & 0x1) << 4) + (abData[dwLoop + 2] >> 4);// DDDDD
+                        acPart[3] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 2] & 0xF) << 1) + (abData[dwLoop + 3] >> 7);// EEEEE
+                        acPart[4] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 3] & 0x7F) >> 2; // FFFFF
+                        acPart[5] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 3] & 0x3) << 3; // GG000
+                        acPart[6] = m_acBase32Map[dwCharIndex];
+                        dwCharCount = 7;
+                        break;
+
+                    default:  // >= 5
+                        dwCharIndex = abData[dwLoop] >> 3;    // AAAAA
+                        acPart[0] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop] & 0x7) << 2) + (abData[dwLoop + 1] >> 6); // BBBBB
+                        acPart[1] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 1] & 0x3F) >> 1; // CCCCC
+                        acPart[2] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 1] & 0x1) << 4) + (abData[dwLoop + 2] >> 4);// DDDDD
+                        acPart[3] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 2] & 0xF) << 1) + (abData[dwLoop + 3] >> 7);// EEEEE
+                        acPart[4] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = (abData[dwLoop + 3] & 0x7F) >> 2; // FFFFF
+                        acPart[5] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = ((abData[dwLoop + 3] & 0x3) << 3) + (abData[dwLoop + 4] >> 5);// GGGGG
+                        acPart[6] = m_acBase32Map[dwCharIndex];
+                        dwCharIndex = abData[dwLoop + 4] & 0x1F;  // HHHHH
+                        acPart[7] = m_acBase32Map[dwCharIndex];
+                        dwCharCount = 8;
+                        break;
+                }
+
+                sbOutput.Append(acPart, 0, dwCharCount);
+            }
+
+            return sbOutput.ToString();
+        }
+
+        public static Byte[] Decode(String sData)
+        {
+            Int32 dwLoop = 0, dwLength = 0;
+            Int32[] dwCharIndex = null;
+            Byte[] abOutput = null;
+            Char[] acInput = null;
+
+            if (sData == null || sData == String.Empty)
+                return null;
+
+            acInput = sData.ToCharArray();
+            if (acInput == null)
+                return null;
+
+            try
+            {
+                dwLength = (acInput.Length / 8 * 5) + 1;
+                abOutput = new Byte[dwLength];
+                dwCharIndex = new Int32[8];
+            }
+            catch (Exception)
+            {
+            }
+            if (acInput == null)
+                return null;
+
+            dwLength = 0;
+            for (dwLoop = 0; dwLoop < acInput.Length; dwLoop += 8)
+            {
+                switch (acInput.Length - dwLoop)
+                {
+                    case 1:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3);
+                        break;
+
+                    case 2:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6);
+                        break;
+
+                    case 3:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1);
+                        break;
+
+                    case 4:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+                        dwCharIndex[3] = GetCharIndex(acInput[dwLoop + 3]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1 + dwCharIndex[3] >> 4);
+                        abOutput[dwLength + 2] = (Byte)((dwCharIndex[3] & 0xF) << 4);
+                        break;
+
+                    case 5:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+                        dwCharIndex[3] = GetCharIndex(acInput[dwLoop + 3]);
+                        dwCharIndex[4] = GetCharIndex(acInput[dwLoop + 4]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1 + dwCharIndex[3] >> 4);
+                        abOutput[dwLength + 2] = (Byte)((dwCharIndex[3] & 0xF) << 4 + dwCharIndex[4] >> 1);
+                        abOutput[dwLength + 3] = (Byte)((dwCharIndex[4] & 0x1) << 7);
+                        break;
+
+                    case 6:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+                        dwCharIndex[3] = GetCharIndex(acInput[dwLoop + 3]);
+                        dwCharIndex[4] = GetCharIndex(acInput[dwLoop + 4]);
+                        dwCharIndex[5] = GetCharIndex(acInput[dwLoop + 5]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1 + dwCharIndex[3] >> 4);
+                        abOutput[dwLength + 2] = (Byte)((dwCharIndex[3] & 0xF) << 4 + dwCharIndex[4] >> 1);
+                        abOutput[dwLength + 3] = (Byte)((dwCharIndex[4] & 0x1) << 7 + dwCharIndex[5] << 2);
+                        break;
+
+                    case 7:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+                        dwCharIndex[3] = GetCharIndex(acInput[dwLoop + 3]);
+                        dwCharIndex[4] = GetCharIndex(acInput[dwLoop + 4]);
+                        dwCharIndex[5] = GetCharIndex(acInput[dwLoop + 5]);
+                        dwCharIndex[6] = GetCharIndex(acInput[dwLoop + 6]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1 + dwCharIndex[3] >> 4);
+                        abOutput[dwLength + 2] = (Byte)((dwCharIndex[3] & 0xF) << 4 + dwCharIndex[4] >> 1);
+                        abOutput[dwLength + 3] = (Byte)((dwCharIndex[4] & 0x1) << 7 + dwCharIndex[5] << 2 + dwCharIndex[6] >> 3);
+                        abOutput[dwLength + 4] = (Byte)((dwCharIndex[6] & 0x7) << 5);
+                        break;
+
+                    default:
+                        dwCharIndex[0] = GetCharIndex(acInput[dwLoop]);
+                        dwCharIndex[1] = GetCharIndex(acInput[dwLoop + 1]);
+                        dwCharIndex[2] = GetCharIndex(acInput[dwLoop + 2]);
+                        dwCharIndex[3] = GetCharIndex(acInput[dwLoop + 3]);
+                        dwCharIndex[4] = GetCharIndex(acInput[dwLoop + 4]);
+                        dwCharIndex[5] = GetCharIndex(acInput[dwLoop + 5]);
+                        dwCharIndex[6] = GetCharIndex(acInput[dwLoop + 6]);
+                        dwCharIndex[7] = GetCharIndex(acInput[dwLoop + 7]);
+
+                        abOutput[dwLength] = (Byte)(dwCharIndex[0] << 3 + dwCharIndex[1] >> 2);
+                        abOutput[dwLength + 1] = (Byte)((dwCharIndex[1] & 0x3) << 6 + dwCharIndex[2] << 1 + dwCharIndex[3] >> 4);
+                        abOutput[dwLength + 2] = (Byte)((dwCharIndex[3] & 0xF) << 4 + dwCharIndex[4] >> 1);
+                        abOutput[dwLength + 3] = (Byte)((dwCharIndex[4] & 0x1) << 7 + dwCharIndex[5] << 2 + dwCharIndex[6] >> 3);
+                        abOutput[dwLength + 4] = (Byte)((dwCharIndex[6] & 0x7) << 5 + dwCharIndex[8]);
+                        break;
+                }
+                dwLength += 5;
+            }
+
+            return abOutput;
+        }
+    }
+}
---
+++ Base32Code/Base32Code/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base32Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int a = random.Next(262143);
+            int b = random.Next(262143);
+            var byteA = GetByteArry(a);
+            var byteB = GetByteArry(b);
+            Byte[] ab = new Byte[5];
+            ab[1] = byteA[2];
+            ab[2] = byteA[3];
+            ab[3] = byteB[2];
+            ab[4] = byteB[3];
+            int temp5 = (int)(byteA[1]) * 4 + (int)byteB[1];
+            var arry4 = GetEachBitInt(ab[4]);
+            var arry3 = GetEachBitInt(ab[3]);
+            var arry2 = GetEachBitInt(ab[2]);
+            var arry1 = GetEachBitInt(ab[1]);
+            var arry0 = GetEachBitInt(temp5);
+            int high0 = (arry0[4] + arry1[4] + arry1[7] + arry2[4] + arry2[7] + arry3[4] + arry3[7] + arry4[4] + arry4[7]) % 2;
+            int high1 = (arry0[3] + arry1[3] + arry1[6] + arry2[3] + arry2[6] + arry3[3] + arry3[6] + arry4[3] + arry4[6]) % 2;
+            int high2 = (arry0[2] + arry1[2] + arry1[5] + arry2[2] + arry2[5] + arry3[2] + arry3[5] + arry4[2] + arry4[5]) % 2;
+            int high3 = (arry0[1] + arry1[1] + arry1[4] + arry2[1] + arry2[4] + arry3[1] + arry3[4] + arry4[1] + arry4[4]) % 2;
+            temp5 = temp5 + high0 * 128 + high1 * 64 + high2 * 32 + high3 * 16;
+            ab[0] = Byte.Parse(temp5.ToString());
+            //Console.WriteLine("{0},{1}, {2}", a, b);
+            string result = CBase32.Encode(ab);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 将32为int转换为字节数组
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static byte[] GetByteArry(int n)
+        {
+            byte[] b = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                b[i] = (byte)(n >> (24 - i * 8));
+                //Console.WriteLine("{0}, {1}, {2}", n, b[i], i);
+            }
+            return b;
+        }
+
+        /// <summary>
+        /// 获取低8位的每一个bit
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int[] GetEachBitInt(int num)
+        {
+            int[] result = new int[8];
+            result[7] = (num & 0x01) == 0x01 ? 1 : 0;
+            result[6] = (num & 0x02) == 0x02 ? 1 : 0;
+            result[5] = (num & 0x04) == 0x04 ? 1 : 0;
+            result[4] = (num & 0x08) == 0x08 ? 1 : 0;
+            result[3] = (num & 0x10) == 0x10 ? 1 : 0;
+            result[2] = (num & 0x20) == 0x20 ? 1 : 0;
+            result[1] = (num & 0x40) == 0x40 ? 1 : 0;
+            result[0] = (num & 0x80) == 0x80 ? 1 : 0;
+            return result;
+        }
+    }
+}
