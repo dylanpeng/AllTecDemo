feb6242730ac0adab7e2390400b28f489b1ac251
peng_dylan <peng_dylan@sina.com>
2017-03-31 23:24:29 +0800

    2017-3-31 memcached实例

---
+++ MemcachedTest/MemcachedTest/MemcachedHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enyim.Caching;
+
+namespace MemcachedTest
+{
+    public class MemcachedHelper
+    {
+        MemcachedClient memClient = new MemcachedClient();
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Set(string key, object value)
+        {
+            object memValue;
+            if (memClient.TryGet(key, out memValue))
+            {
+                memClient.Remove(key);
+            }
+            return memClient.Store(Enyim.Caching.Memcached.StoreMode.Add, key, value);
+        }
+
+        /// <summary>
+        /// 获取缓存 泛型方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T Get<T>(string key) where T : class
+        {
+            object result;
+            if (memClient.TryGet(key, out result))
+                return result as T;
+            return null;
+        }
+
+        /// <summary>
+        /// 获取缓存 通用方法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object Get(string key)
+        {
+            object result;
+            if (memClient.TryGet(key, out result))
+                return result;
+            return null;
+        }
+    }
+}
---
+++ MemcachedTest/MemcachedTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enyim.Caching;
+
+namespace MemcachedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MemcachedHelper memHelper = new MemcachedHelper();
+                //缓存字符串
+                memHelper.Set("memStr", "Hello World!");
+                var a = memHelper.Get("memStr");
+                Console.WriteLine(a);
+                //缓存类型
+                memHelper.Set("memClass", new MemClass() { Id = 1, Name = "class" });
+                var b = memHelper.Get<MemClass>("memClass");
+                Console.WriteLine(string.Format("Id:{0} Name:{1}", b.Id, b.Name));
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+
+
+    [Serializable]//注：缓存类型一定要加可序列化的特性，否则无法缓存成功
+    public class MemClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
