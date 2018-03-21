5a891de5fe220964a6033e306943de6ec20bf581
peng_dylan <peng_dylan@sina.com>
2018-03-21 22:33:46 +0800

    2018-3-21 rabbitmqtest push

---
+++ RabbitMQTest/RabbitMQTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////发送方
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        channel.QueueDeclare(queue: "hello",
+            //                             durable: false,
+            //                             exclusive: false,
+            //                             autoDelete: false,
+            //                             arguments: null);
+
+            //        string message = "Hello World!";
+            //        var body = Encoding.UTF8.GetBytes(message);
+
+            //        channel.BasicPublish(exchange: "",
+            //                             routingKey: "hello",
+            //                             basicProperties: null,
+            //                             body: body);
+            //        Console.WriteLine(" [x] Sent {0}", message);
+            //    }
+            //}
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+
+            //接受方
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
